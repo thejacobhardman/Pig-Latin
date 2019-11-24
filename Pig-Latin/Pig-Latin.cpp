701b8c40f80a0ca242ef23e147a4f80beabdeb83
Jacob Hardman <42230736+thejacobhardman@users.noreply.github.com>
2019-11-23 19:02:59 -0700

    add basic program loop

--- Pig-Latin/Pig-Latin.cpp
+++ Pig-Latin/Pig-Latin.cpp
@@ -4,10 +4,35 @@
 // 11/23/19
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+string convertString(string stringToCovert) {
+	string convertedString;
+
+
+
+	return convertedString;
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	string userInput, pigLatinString;
+
+	cout << "Enter a sentence and I will convert it to Pig Latin." << endl;
+
+	do {
+		cout << "-> ";
+		getline(cin, userInput);
+		cout << userInput;
+		if (userInput == "exit" || userInput == "Exit") { break; }
+
+		pigLatinString = convertString(userInput);
+
+		cout << pigLatinString << endl;
+
+	} while (userInput != "exit" || userInput != "Exit");
+
+	cout << endl;
 }
