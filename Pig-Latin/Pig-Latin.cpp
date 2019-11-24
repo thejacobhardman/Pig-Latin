dcc407dc9064ef72482059539f52a8c16ff5c325
Jacob Hardman <42230736+thejacobhardman@users.noreply.github.com>
2019-11-23 21:02:36 -0700

    add pig latin conversions

--- Pig-Latin/Pig-Latin.cpp
+++ Pig-Latin/Pig-Latin.cpp
@@ -5,13 +5,45 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
+#include <sstream>
 
 using namespace std;
 
-string convertString(string stringToCovert) {
+string convertString(string stringToConvert) {
 	string convertedString;
+	vector<string> container;
+	const char VOWELS[10] = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+	bool isFirstLetterVowel = false;
+	
+	istringstream ss(stringToConvert);
+	do {
+		string word;
+		ss >> word;
+		container.push_back(word);
+	} while (ss);
 
-
+	for (int i = 0; i < container.size(); i++) {
+		if (container[i] != "") {
+			for (int j = 0; j < 10; j++) {
+				isFirstLetterVowel = false;
+				if (container[i].at(0) == VOWELS[j]) {
+					isFirstLetterVowel = true;
+					break;
+				}
+			}
+			if (isFirstLetterVowel == true) {
+				container[i].insert(container[i].size(), "ay");
+				convertedString += (container[i] + " ");
+			}
+			else {
+				container[i] += container[i].at(0);
+				container[i].erase(0, 1);
+				container[i].insert(container[i].size(), "ay");
+				convertedString += (container[i] + " ");
+			}
+		}
+	}
 
 	return convertedString;
 }
@@ -21,11 +53,11 @@ int main()
 	string userInput, pigLatinString;
 
 	cout << "Enter a sentence and I will convert it to Pig Latin." << endl;
+	cout << "Type 'Exit' to quit the program." << endl;
 
 	do {
 		cout << "-> ";
 		getline(cin, userInput);
-		cout << userInput;
 		if (userInput == "exit" || userInput == "Exit") { break; }
 
 		pigLatinString = convertString(userInput);
@@ -33,6 +65,4 @@ int main()
 		cout << pigLatinString << endl;
 
 	} while (userInput != "exit" || userInput != "Exit");
-
-	cout << endl;
 }
